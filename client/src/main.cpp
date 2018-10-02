27d50a09f25e2c6e0f4c65fc25f0ec222d5d3bb8
Connor Gallimore <connor.gallimore@epitech.eu>
2018-10-02 19:28:35 +0200

    Fonts for qt

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,12 +1,20 @@
 #include "MainWindow.hpp"
 #include <CallWindow.hpp>
 #include <QtWidgets/QApplication>
+#include <QtGui/QFontDatabase>
+#include <QtCore/QFile>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+	QApplication a(argc, argv);
 
-    return a.exec();
+	int id = QFontDatabase::addApplicationFont("../../fonts/arial.ttf");
+	QString family = QFontDatabase::applicationFontFamilies(id).at(0);
+	QFont ariel(family);
+
+	MainWindow w;
+	w.show();
+
+	return a.exec();
 }
