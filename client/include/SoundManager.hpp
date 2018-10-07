07b7d56d6e6a8dfbe7bc998fb055b2741d4c65b1
Connor Gallimore <connor.gallimore@epitech.eu>
2018-10-07 15:14:16 +0200

    Added opus encoding decoding

---
+++ client/include/SoundManager.hpp
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2018
+** Babel
+** File description:
+** Sound manager
+*/
+
+#pragma once
+
+#include "UdpClient.hpp"
+#include "CodecManager.hpp"
+#include "portaudio.h"
+
+#define SAMPLE_RATE  (48000)
+#define FRAMES_PER_BUFFER (60 * SAMPLE_RATE / 1000)
+#define NUM_CHANNELS    (1)
+
+#define PA_SAMPLE_TYPE  paInt16
+#define SAMPLE_SIZE sizeof(short)
+#define SAMPLE_SILENCE  (0)
+
+class SoundManager {
+	public:
+		SoundManager(udpclient::UdpClient *);
+		~SoundManager();
+
+		int paCallback(const void*, void*, unsigned long, const PaStreamCallbackTimeInfo*, PaStreamCallbackFlags, void*);
+	private:
+		udpclient::UdpClient *_udpClient;
+		CodecManager *_codecManager;
+
+};
